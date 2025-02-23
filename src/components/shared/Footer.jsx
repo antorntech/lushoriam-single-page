b1ead5abf197c877f117cea33c489c544fbb74d2
antor10655gh <creativefreelancerantor@gmail.com>
2025-02-24 05:34:42 +0600

    update the footer

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -8,8 +8,8 @@ const Footer = () => {
       <Container>
         <div className="flex flex-col md:flex-row gap-2 md:gap-0 justify-between items-center">
           <div className="text-white flex items-center gap-1">
-            <Location className="size-6" color="currentColor" />
-            <h1 className="font-semibold text-md">
+            <Location className="size-5 md:size-6" color="currentColor" />
+            <h1 className="font-semibold text-sm md:text-md">
               ঠিকানা: লালমাটিয়া, মোহাম্মদপুর, ঢাকা
             </h1>
           </div>
@@ -18,7 +18,7 @@ const Footer = () => {
               <li>
                 <a
                   href=""
-                  className="flex items-center gap-1 hover:text-primary transition-all duration-300"
+                  className="flex items-center gap-1 text-sm md:text-md hover:text-primary transition-all duration-300"
                 >
                   <Link className="size-4" color="currentColor" />
                   <span>Privacy Policy</span>
@@ -27,7 +27,7 @@ const Footer = () => {
               <li>
                 <a
                   href=""
-                  className="flex items-center gap-1 hover:text-primary transition-all duration-300"
+                  className="flex items-center gap-1 text-sm md:text-md hover:text-primary transition-all duration-300"
                 >
                   <Link className="size-4" color="currentColor" />
                   <span>Terms & Conditions</span>
