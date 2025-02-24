c61ede2fed611cadf77830401b716d8d930bee7b
antor10655gh <creativefreelancerantor@gmail.com>
2025-02-24 06:43:06 +0600

    hotfix the component

--- src/components/shared/Accordion.jsx
+++ src/components/shared/Accordion.jsx
@@ -37,14 +37,14 @@ const Accordion = () => {
   };
 
   return (
-    <div className="w-full max-w-2xl mx-auto mt-5 md:mt-10">
+    <div className="w-full max-w-2xl mx-auto mt-2 md:mt-8">
       {faqData.map((item, index) => (
         <div key={index} className="border-b border-gray-300">
           <button
             className="w-full flex justify-between items-center p-4 text-left font-medium bg-primary/0 hover:bg-primary/10 transition-all duration-300"
             onClick={() => toggleAccordion(index)}
           >
-            <span className="font-bold text-md md:text-md">
+            <span className="font-bold text-sm lg:text-md">
               {item.question}
             </span>
             {openIndex === index ? (
@@ -61,7 +61,7 @@ const Accordion = () => {
           </button>
           <div
             className={`overflow-hidden transition-all duration-300 ease-in-out ${
-              openIndex === index ? "max-h-40 p-4" : "max-h-0 p-0"
+              openIndex === index ? "max-h-80 p-4" : "max-h-0 p-0"
             }`}
           >
             <p className="text-gray-700">{item.answer}</p>
