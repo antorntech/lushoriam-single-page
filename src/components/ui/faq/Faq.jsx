0915e186803b70dacfb4064f8d6f55c49d5213dd
antor10655gh <creativefreelancerantor@gmail.com>
2025-05-04 17:53:29 +0600

    hotfix

--- src/components/ui/faq/Faq.jsx
+++ src/components/ui/faq/Faq.jsx
@@ -4,7 +4,7 @@ import Accordion from "../../shared/Accordion";
 
 const Faq = () => {
   return (
-    <Container className="bg-primary/10 w-full mt-8 md:mt-16 py-5 md:py-10">
+    <Container className="bg-primary/10 w-full mt-8 md:mt-16 py-5 md:py-10 rounded-md">
       <div className="flex justify-center">
         <h1 className="text-xl md:text-2xl font-bold text-primary">
           প্রায়শই জিজ্ঞাসিত প্রশ্ন
